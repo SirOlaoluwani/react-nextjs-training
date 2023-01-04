29d9c9edac2193a30bdc8e9edca6860697041c58
Olaoluwani Onafowope <olaoluwa@getbumpa.com>
2023-01-04 17:33:16 +0100

    initial commit

---
+++ pages/home/index.js
@@ -0,0 +1,9 @@
+import "./styles.css"
+
+export default function Home() {
+    return (
+        <div className="home">
+            <p>I am yellow</p>
+        </div>
+    )
+}
---
+++ pages/money/index.js
@@ -0,0 +1,9 @@
+import "./money.css"
+
+export default function Money() {
+    return (
+        <div className="home">
+            <p>I am red</p>
+        </div>
+    )
+}
